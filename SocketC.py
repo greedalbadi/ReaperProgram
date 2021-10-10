ad962dd5b0d1d74672c1c25c70d7b5f7e56eaded
greed <greedalbadi@gmail.com>
2021-10-11 01:48:36 +0400

    [FET] added timer's

--- SocketC.py
+++ SocketC.py
@@ -1,6 +1,6 @@
-import os
 import socket
 import threading
+import time
 from socket import *
 from IPy import IP
 class SocketCommands:
@@ -15,13 +15,15 @@ class SocketCommands:
                 return e
         def PortScanner(self, user_input):
             try:
+                self.end = False
                 target = SocketCommands.Checkip(self, user_input[1])
                 target = gethostbyname(target)
                 start_range = user_input[2]
                 end_range = user_input[3]
                 print('scan started', target)
                 for port in range(int(start_range), int(end_range)):
-                    threading.Thread(target=SocketCommands.Scan, args=[self, port, target]).start()
+                    if self.end != True:
+                        threading.Thread(target=SocketCommands.Scan, args=[self, port, target]).start()
             except Exception as e:
                 return e
         def Scan(self, port, target):
@@ -33,7 +35,8 @@ class SocketCommands:
                 print(f'[+]Open port: {port}')
             s.close()
             self.active_thread = self.active_thread - 1
-            if self.active_thread == 0:
+            if self.active_thread == 0 and self.end == False:
+                self.end = True
                 print("Search ended.")
         def Checkip(self, ip):
             try:
@@ -67,16 +70,21 @@ class SocketCommands:
             except Exception as e:
                 return e
         def Focus(self, user_input):
-            try:
-                target = user_input[1]
-                port = user_input[2]
-                print("focusing..")
-                while True:
-                    try:
-                        s = socket(AF_INET, SOCK_STREAM)
-                        s.connect_ex((target, int(port)))
-                        return f"server: {target} | port: {port} | is live"
-                    except Exception as e:
-                        print(e)
-            except Exception as e:
-                return e
+            def fuc(user_input):
+                try:
+                    start = time.time()
+                    target = user_input[1]
+                    port = user_input[2]
+                    print(f"focusing daemon thread started at host: {target}| port: {port}..")
+                    while True:
+                        try:
+                            s = socket(AF_INET, SOCK_STREAM)
+                            conn = s.connect_ex((target, int(port)))
+                            if (conn == 0):
+                                print(f"server: {target} | port: {port} | is live|Focus time {time.time() - start}")
+                                break
+                        except Exception as e:
+                            print(e)
+                except Exception as e:
+                    return e
+            return threading.Thread(target=fuc, args=[user_input], daemon=True).start()
