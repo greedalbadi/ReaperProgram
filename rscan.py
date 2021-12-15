51bc59400cfc5b3eece9327ce6e46abc288dc7cb
greed <greedalbadi@gmail.com>
2021-12-16 03:26:14 +0400

    [FET] scan for RDP & MAC address

--- rscan.py
+++ rscan.py
@@ -4,19 +4,32 @@ import requests
 import socket
 from prettytable import PrettyTable
 import os
+from getmac import get_mac_address
 class Scan_info:
     shell = True
     Prot = "http://"
+    RDP_port = 3389
 class Ip_range_scanner:
     def __init__(self):
         self.os = os.name
         self.Activethreads = 0
-        self.table = PrettyTable(["address", "hostname", "Browser"])
+        self.table = PrettyTable(["Address", "Hostname", "Browser", "RDP", "MAC"])
+    def Rdpcheck(self, target, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(3)
+        conn = s.connect_ex((target, port))
+        if (conn == 0):
+            return True
+        else:
+            return False
     def command(self, ip):
         if self.os == "nt":
             return f"ping {ip} -n 1"
         else:
             return f"ping {ip} -c 1"
+    def Getmac(self, ip):
+        address = get_mac_address(ip=ip)
+        return address
     def scanner(self, ip, rang):
         print("Scanning network..")
         ip = ip[:-1]
@@ -50,5 +63,7 @@ class Ip_range_scanner:
         if res.returncode == 0:
             browser = Ip_range_scanner.Checkbrowser(self, ip)
             hostname = Ip_range_scanner.Gethostname(self, ip)
-            self.table.add_row([ip, hostname, browser])
+            RDP = Ip_range_scanner.Rdpcheck(self, ip, Scan_info.RDP_port)
+            mac = Ip_range_scanner.Getmac(self, ip)
+            self.table.add_row([ip, hostname, browser, RDP, mac])
         self.Activethreads -= 1
